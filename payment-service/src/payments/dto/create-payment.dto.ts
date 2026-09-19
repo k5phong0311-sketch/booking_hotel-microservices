2e71c87830ac3c80065c19d2c89ee621e8de7894
Đậu Ngọc Anh <daungocanh90@gmail.com>
2026-09-19 11:30:00 +0700

    feat: add PaymentMethod enum (CASH/CARD/TRANSFER) to CreatePaymentDto

--- payment-service/src/payments/dto/create-payment.dto.ts
+++ payment-service/src/payments/dto/create-payment.dto.ts
@@ -1,12 +1,30 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsEnum, IsOptional, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+// Phương thức thanh toán được hỗ trợ
+export enum PaymentMethod {
+  CASH     = 'CASH',      // Tiền mặt tại quầy
+  CARD     = 'CARD',      // Thẻ ngân hàng
+  TRANSFER = 'TRANSFER',  // Chuyển khoản
+}
 
 export class CreatePaymentDto {
+  @Type(() => Number)
   @IsNumber()
+  @Min(1)
   bookingId: number;
 
+  @Type(() => Number)
   @IsNumber()
+  @Min(1)
   userId: number;
 
+  @Type(() => Number)
   @IsNumber()
+  @Min(1, { message: 'Số tiền thanh toán phải lớn hơn 0' })
   amount: number;
+
+  @IsEnum(PaymentMethod, { message: 'Phương thức thanh toán không hợp lệ' })
+  @IsOptional()
+  method?: PaymentMethod = PaymentMethod.TRANSFER;
 }
